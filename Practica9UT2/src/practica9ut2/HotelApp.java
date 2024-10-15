1f4029eb033211a5c3e4258924176a029ceb35c4
Daniel Monforte Ibias <daniel.monforteibias@riberadeltajo.es>
2024-10-15 16:14:55 +0200

    15/10/2024 commit
    Commit including the first steps for "Practica9UT2"
    TODO:
        - Finish "Practica7UT2"
        - Add logic to "Practica9UT2"

---
+++ Practica9UT2/src/practica9ut2/HotelApp.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica9ut2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class HotelApp {
+    public static void main(String[] args) {
+        VentanaHotel ventana=new VentanaHotel();
+        ventana.setVisible(true);
+    }
+}
