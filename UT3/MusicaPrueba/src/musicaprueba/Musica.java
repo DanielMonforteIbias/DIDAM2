93164fd99fa3171674fe92c4630cdba9d189c57a
Daniel Monforte Ibias <daniel.monforteibias@riberadeltajo.es>
2024-10-28 20:32:05 +0100

    28/10/2024 commit
    Additions:
        - MusicaPrueba has been finished
        - Practica6UT3 has been started
    TODO:
        - Finish Practica6UT3 (I need to add sound)
        - Keep developing Practica5UT3
    NOTES:
        - I need to upload Actividad3.1

---
+++ UT3/MusicaPrueba/src/musicaprueba/Musica.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package musicaprueba;
+
+/**
+ *
+ * @author Tarde
+ */
+public class Musica {
+    public static void main(String[] args) {
+        Audio repro=new Audio();
+        repro.setVisible(true);
+    }
+}
