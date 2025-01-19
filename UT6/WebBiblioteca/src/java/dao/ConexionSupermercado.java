07d7e36c6c88ccb564151cdd119876710908eb9f
Daniel Monforte Ibias <daniel.monforteibias@riberadeltajo.es>
2025-01-19 16:00:24 +0100

    Practica3UT6 and WebBiblioteca commit
    Additions:
        - I finished Practica3UT6 and the 4th activity (creating WebBiblioteca)
    
    TODO:
        - Install XAMPP and Tomcat on the class computer (I installed it at home)

---
+++ UT6/WebBiblioteca/src/java/dao/ConexionSupermercado.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Danim2205
+ */
+public class ConexionSupermercado {
+    public static Connection conectar(){
+        try {
+            Class.forName("com.mysql.jbdc.Driver");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/supermercado", "root", "");
+        } catch (ClassNotFoundException ex) {
+            return null;
+        } catch (SQLException ex) {
+            return null;
+        }
+    }
+}
